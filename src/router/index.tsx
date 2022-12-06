b3741b81bd23c7e227b4141f6e9eac4a7db41492
JuliaLen-Web <Juli.98@list.ru>
2022-12-06 19:10:37 +0100

    project cleaning

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,44 +9,17 @@ import TransactionList from "../pages/TransactionList";
 import TransactionDetail from "../pages/TransactionDetail";
 import SellerList from "../pages/SellerList";
 import SellerDetail from "../pages/SellerDetail";
-import Reviews from "../pages/Reviews";
-import FileManager from "../pages/FileManager";
 import PointOfSale from "../pages/PointOfSale";
-import Post from "../pages/Post";
-import Calendar from "../pages/Calendar";
-import CrudDataList from "../pages/CrudDataList";
 import CrudForm from "../pages/CrudForm";
 import UsersPage from "../pages/UsersPage";
 import ProfileUser from "../pages/ProfileUser";
-import FaqLayout1 from "../pages/FaqLayout1";
-import FaqLayout2 from "../pages/FaqLayout2";
-import FaqLayout3 from "../pages/FaqLayout3";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ErrorPage from "../pages/ErrorPage";
 import ProfileSettings from "../pages/ProfileSettings";
-import RegularTable from "../pages/RegularTable";
-import Tabulator from "../pages/Tabulator";
-import Modal from "../pages/Modal";
-import SlideOver from "../pages/SlideOver";
 import Notification from "../pages/Notification";
-import Tab from "../pages/Tab";
-import Accordion from "../pages/Accordion";
-import Alert from "../pages/Alert";
-import ProgressBar from "../pages/ProgressBar";
-import Tooltip from "../pages/Tooltip";
-import Dropdown from "../pages/Dropdown";
-import Typography from "../pages/Typography";
-import Icon from "../pages/Icon";
-import LoadingIcon from "../pages/LoadingIcon";
 import RegularForm from "../pages/RegularForm";
-import Datepicker from "../pages/Datepicker";
-import TomSelect from "../pages/TomSelect";
-import FileUpload from "../pages/FileUpload";
 import Validation from "../pages/Validation";
-import Chart from "../pages/Chart";
-import Slider from "../pages/Slider";
-import ImageZoom from "../pages/ImageZoom";
 
 function Router() {
   const routes = [
@@ -91,28 +64,8 @@ function Router() {
           element: <SellerDetail />,
         },
         {
-          path: "reviews",
-          element: <Reviews />,
-        },
-        {
-          path: "file-manager",
-          element: <FileManager />,
-        },
-        {
           path: "point-of-sale",
           element: <PointOfSale />,
-        },
-        {
-          path: "post",
-          element: <Post />,
-        },
-        {
-          path: "calendar",
-          element: <Calendar />,
-        },
-        {
-          path: "crud-data-list",
-          element: <CrudDataList />,
         },
         {
           path: "crud-form",
@@ -127,108 +80,20 @@ function Router() {
           element: <ProfileUser />,
         },
         {
-          path: "faq-layout-1",
-          element: <FaqLayout1 />,
-        },
-        {
-          path: "faq-layout-2",
-          element: <FaqLayout2 />,
-        },
-        {
-          path: "faq-layout-3",
-          element: <FaqLayout3 />,
-        },
-        {
           path: "profile-settings",
           element: <ProfileSettings />,
-        },
-        {
-          path: "regular-table",
-          element: <RegularTable />,
-        },
-        {
-          path: "tabulator",
-          element: <Tabulator />,
-        },
-        {
-          path: "modal",
-          element: <Modal />,
-        },
-        {
-          path: "slide-over",
-          element: <SlideOver />,
         },
         {
           path: "notification",
           element: <Notification />,
         },
         {
-          path: "tab",
-          element: <Tab />,
-        },
-        {
-          path: "accordion",
-          element: <Accordion />,
-        },
-        {
-          path: "alert",
-          element: <Alert />,
-        },
-        {
-          path: "progress-bar",
-          element: <ProgressBar />,
-        },
-        {
-          path: "tooltip",
-          element: <Tooltip />,
-        },
-        {
-          path: "dropdown",
-          element: <Dropdown />,
-        },
-        {
-          path: "typography",
-          element: <Typography />,
-        },
-        {
-          path: "icon",
-          element: <Icon />,
-        },
-        {
-          path: "loading-icon",
-          element: <LoadingIcon />,
-        },
-        {
           path: "regular-form",
           element: <RegularForm />,
         },
         {
-          path: "datepicker",
-          element: <Datepicker />,
-        },
-        {
-          path: "tom-select",
-          element: <TomSelect />,
-        },
-        {
-          path: "file-upload",
-          element: <FileUpload />,
-        },
-        {
           path: "validation",
           element: <Validation />,
-        },
-        {
-          path: "chart",
-          element: <Chart />,
-        },
-        {
-          path: "slider",
-          element: <Slider />,
-        },
-        {
-          path: "image-zoom",
-          element: <ImageZoom />,
         },
       ],
     },
