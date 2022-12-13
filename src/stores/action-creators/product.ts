72e1644ac6f12d52881aa57e2e5b553193725761
JuliaLen-Web <Juli.98@list.ru>
2022-12-13 15:49:34 +0100

    write draft for display products

--- src/stores/action-creators/product.ts
+++ src/stores/action-creators/product.ts
@@ -1,31 +1,53 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import _ from "lodash";
 import { Products } from "../../types/product";
+import { userRoles } from "../../types/user";
 import fakerData from "../../utils/faker"
 import { RootState } from "../store";
 
-const productsArray: Products = []
-
-fakerData.slice(0, 20).map((faker, fakerKey) => {
-    productsArray.push({
-        id: fakerKey + 1,
-        name: faker.products[0].name,
-        seller: faker.users[0].name,
-        price: Math.ceil(Math.random() * 1000),
-        stock: faker.trueFalse[0],
-        image: "/src/assets/images/fakers/preview-" + Math.ceil(Math.random() * 15) + ".jpg",
-        status: faker.statusProduct[0],
-        category: faker.products[0].category,
-        description: faker.products[1].name + faker.users[1].name + 'bla',
-        featured: faker.trueFalse[0],
-    })
-})
-
 export const fetchProducts = createAsyncThunk(
     'products/fetchProducts',
-    async (): Promise<Products> => {
+    async (_, { getState }): Promise<Products> => {
+
+        const productsArray: Products = []
+
+        fakerData.slice(0, 20).map((faker, fakerKey) => {
+            productsArray.push({
+                id: fakerKey + 1,
+                name: faker.products[0].name,
+                seller: faker.users[0].name,
+                price: Math.ceil(Math.random() * 1000),
+                stock: faker.trueFalse[0],
+                image: "/src/assets/images/fakers/preview-" + Math.ceil(Math.random() * 15) + ".jpg",
+                status: faker.statusProduct[0],
+                category: faker.products[0].category,
+                description: faker.products[1].name + faker.users[1].name + 'bla',
+                featured: faker.trueFalse[0],
+            })
+        })
+
+        const { user } = getState() as RootState
+        const userInfo = user.user
+
+        let newProductsArray: Products = []
+
+        switch (userInfo.role) {
+            case userRoles.Manager:
+                newProductsArray = productsArray.filter(product => product.status === "approved")
+                break
+            case userRoles.Seller:
+                newProductsArray = productsArray.filter(product => product.seller === userInfo.name)
+                break
+            case userRoles.Customer:
+                newProductsArray = productsArray.filter(product => product.status === "approved" && product.stock)
+                break
+            default:
+                newProductsArray = productsArray
+                break
+        }
+
         return new Promise((resolve) => {
-            setTimeout(() => resolve(productsArray), 1000);
+            setTimeout(() => resolve(newProductsArray), 1000);
         });
     }
 )
